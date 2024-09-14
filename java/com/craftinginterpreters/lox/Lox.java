a9995b50e1a85eb1fe822f7ea3d32e71106b60e9
Yash <yashchaurasia630@gmail.com>
2024-09-14 15:07:04 +0530

    started the jlox scanner

---
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Lox {
+  public static void main(String[] args) throws IOException {
+    
+  }
+}
