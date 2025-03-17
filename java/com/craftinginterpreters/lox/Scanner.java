ca0c566ab80fa3749783351734c88bdcdc20ab5a
YASH <yashchaurasia630@gmail.com>
2025-03-17 19:30:26 +0530

    numbers

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -92,9 +92,26 @@ class Scanner {
         string();
         break;
       default:
-        Lox.error(line, "Unexpected character");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character");
+        }
         break;
     }
+  }
+
+  private void number() {
+    while (isDigit(peek()))
+      advance();
+
+    if (peek() == '.' && isDigit(peekNext())) {
+      advance();
+      while (isDigit(peek()))
+        advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
   }
 
   private void string() {
@@ -130,6 +147,16 @@ class Scanner {
     if (isAtEnd())
       return '\0';
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length())
+      return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
   private boolean isAtEnd() {
