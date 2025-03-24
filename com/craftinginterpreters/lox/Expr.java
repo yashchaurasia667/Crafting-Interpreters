51edcc7c3ed98809267e79d93ddb63df6eda0051
YASH <yashchaurasia630@gmail.com>
2025-03-24 20:48:24 +0530

    visitor patterns

--- com/craftinginterpreters/lox/Expr.java
+++ com/craftinginterpreters/lox/Expr.java
@@ -3,8 +3,8 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
-  static class Binary extends Expr{
-    Binary(Expr left, Token operator, Expr right){ 
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
@@ -13,28 +13,31 @@ abstract class Expr {
     final Expr left;
     final Token operator;
     final Expr right;
-}
-  static class Grouping extends Expr{
-    Grouping(Expr expression){ 
+  }
+
+  static class Grouping extends Expr {
+    Grouping(Expr expression) {
       this.expression = expression;
     }
 
     final Expr expression;
-}
-  static class Literal extends Expr{
-    Literal(Object value){ 
+  }
+
+  static class Literal extends Expr {
+    Literal(Object value) {
       this.value = value;
     }
 
     final Object value;
-}
-  static class Unary extends Expr{
-    Unary(Token operator, Expr right){ 
+  }
+
+  static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
     }
 
     final Token operator;
     final Expr right;
-}
+  }
 }
